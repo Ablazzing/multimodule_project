0f38f42c2e30d166760e79e7054d2540767d87a1
Ablazzing <y2286122@yandex.ru>
2022-10-09 15:24:15 +0300

    моя прелесть

---
+++ calc/src/main/java/org/example/Calc.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Calc {
+    public static void main(String[] args) {
+        System.out.println("Какие-то вычисления");
+        int i = 1;
+        Calc calc2 = new Calc();
+        String myData = "ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss"
+              + "ssssssssssssssssssss";
+
+        if (i == 1) {
+            System.out.println("Приветики");
+        }
+    }
+
+    public static void myMethod(int i, int b) {
+
+    }
+}
